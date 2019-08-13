a04899e846de237367d9033d67c1c7f57cef4ab4
JonRider <jon@rider.net>
2019-08-13 09:29:31 +0530

    templates extended

--- application.py
+++ application.py
@@ -24,8 +24,22 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).rowcount == 0:
-            return render_template("books.html")
+        # Get username and password
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+
+        # Check if usename does not exist
+        if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
+            return render_template("index.html", message="Username doesn't exist!")
+
+        # Check if password is correct then login
+        else:
+            user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
+            if user.password == password:
+                return render_template("books.html")
+            else:
+                return render_template("index.html", message="Invalid Password!")
 
 
     return render_template("index.html")
@@ -38,13 +52,13 @@ def signup():
         password = request.form.get("password")
 
         # Check if usename does not exist
-        #if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
-        #    return render_template("signup.html", message="Username already exists!")
-
-        db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-                {"username": username, "password": password})
-        db.commit()
-        return render_template("books.html", username=username, password=password)
-
+        if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 1:
+            return render_template("signup.html", message="Username already exists!")
+        # Otherwise add it to the database
+        else:
+            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                    {"username": username, "password": password})
+            db.commit()
+            return render_template("books.html", username=username, password=password)
 
     return render_template("signup.html")
